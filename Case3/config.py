edabe5767d85185e346fb1c1771b289868b85d31
DeplorableJed <48975520+DeplorableJed@users.noreply.github.com>
2021-07-20 11:57:32 -0700

    Update config.py

--- Case3/config.py
+++ Case3/config.py
@@ -3,11 +3,6 @@ vmanage_port = "8443"
 vmanage_username = "admin"
 vmanage_password = "Cisco123!"
 
-WEBEX_TEAMS_URL = 'https://webexapis.com/v1'
-WEBEX_TEAMS_AUTH = 'Bearer ' + 'OTM3ZTMxZjItZGM4ZC00ODA5LWI1ZTYtMDAyMmUzOTNhOTNjMjQxM2IxOWYtYjk5_PF84_e17d4b4d-d1d4-4ebd-8b20-7735538ecf7b' # Enter your token good for 12 hours here
-WEBEX_TEAMS_SPACE_NAME = 'Jed SASE Test' # Get the exact name from Webex
-
-
 template_name = 'PodXX_banner_cEdge_1k'
 template_description = 'Banner for PodXX'
 login_banner = 'Hello PodXX'
